795fe3afe49ae43f4fca1053347930dad85ff79b
Ahmad Toriq Afandi <ahmad.toriq@corp.bm.co.id>
2025-09-15 15:17:55 +0700

    add link

--- app/Views/layouts/header_dashboard.php
+++ app/Views/layouts/header_dashboard.php
@@ -1,7 +1,8 @@
 <div class="w-full fixed top-0 left-0 shadow z-50 bg-white">
     <div class="container mx-auto py-4">
         <div class="flex justify-between items-center">
-            <h1 class="text-3xl font-semibold text-blue-700">DM</h1>
+            <h1 class="text-3xl font-semibold text-blue-700"> 
+                <a href="<?= base_url('/dashboard') ?>">DM</a></h1>
             <div class="grid grid-cols-2 gap-4 items-center">
                 <div>
                     <p class="capitalize text-xs">welcome,</p>
