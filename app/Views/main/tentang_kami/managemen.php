02b4a9fa3d5bbfdb1fad9a656dded3036fe220fa
Ahmad Toriq Afandi <ahmad.toriq@corp.bm.co.id>
2025-09-24 16:32:26 +0700

    next 2

--- app/Views/main/tentang_kami/managemen.php
+++ app/Views/main/tentang_kami/managemen.php
@@ -27,30 +27,30 @@
                                         <p class="text-base"><?= esc($p['jabatan']) ?></p>
                                     </div>
                                     <p><?= esc($p['kewarganegaraan']) ?>, lahir di <?= esc($p['tempat_lahir']).' '.date('d F Y', strtotime($p['tanggal_lahir'])) ?> </p>
-                                    <div class="flex flex-1 gap-2">
+                                    <!-- <div class="flex flex-1 gap-2">
                                         <p class="font-bold">Pendidikan:</p>
                                         <ul class="list-outside list-disc ml-5">
                                         <?php
-                                            foreach (json_decode($p['pendidikan']) as $edu) : 
+                                            // foreach (json_decode($p['pendidikan']) as $edu) : 
                                             ?>
-                                                <li><?= esc($edu)?></li>
+                                                <li><?//= esc($edu)?></li>
                                             <?php
-                                            endforeach;
+                                            // endforeach;
                                         ?>
                                         </ul>
-                                    </div>
-                                    <div class="flex flex-col">
+                                    </div> -->
+                                    <!-- <div class="flex flex-col">
                                         <p class="font-bold">Pengalaman Kerja:</p>
                                         <ul class="list-outside list-disc ml-8">
                                         <?php
-                                            foreach (json_decode($p['pengalaman_kerja']) as $work) : 
+                                            // foreach (json_decode($p['pengalaman_kerja']) as $work) : 
                                             ?>
-                                                <li><?= esc($work)?></li>
+                                                <li><?//= esc($work)?></li>
                                             <?php
-                                            endforeach;
+                                            // endforeach;
                                         ?>
                                         </ul>
-                                    </div>
+                                    </div> -->
                                     <!-- <div class="flex flex-col">
                                         <p class="font-bold">Pelatihan:</p>
                                         <ul class="list-outside list-disc ml-8">
