40f38042d90b951952315fb3dfc7c29128cb8bed
Ahmad Toriq Afandi <ahmad.toriq@corp.bm.co.id>
2025-11-24 14:57:44 +0700

    check

--- app/Views/layouts/main.php
+++ app/Views/layouts/main.php
@@ -9,22 +9,22 @@
     <link href="<?= base_url("assets/icon_madinah.ico") ?>" rel="shortcut icon" type="image/x-icon">
 
     <link href="https://cdn.jsdelivr.net/npm/cropperjs@1.5.13/dist/cropper.min.css" rel="stylesheet">
-    <script src="https://cdn.jsdelivr.net/npm/cropperjs@1.5.13/dist/cropper.min.js"></script>
+    <script src="https://cdn.jsdelivr.net/npm/cropperjs@1.5.13/dist/cropper.min.js" defer></script>
 
-    <script src="https://cdn.tailwindcss.com"></script>
+    <script src="https://cdn.tailwindcss.com" defer></script>
     <script src="https://unpkg.com/alpinejs" defer></script>
     <script src="https://kit.fontawesome.com/a7feba845e.js" crossorigin="anonymous"></script>
 
-    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/swiper@11/swiper-bundle.min.css" />
-    <script src="https://cdn.jsdelivr.net/npm/swiper@11/swiper-bundle.min.js"></script>
+    <link rel="preload" href="https://cdn.jsdelivr.net/npm/swiper@11/swiper-bundle.min.css" />
+    <script src="https://cdn.jsdelivr.net/npm/swiper@11/swiper-bundle.min.js" defer></script>
 
     <!-- Leaflet CSS -->
-    <link rel="stylesheet" href="https://unpkg.com/leaflet/dist/leaflet.css" />
+    <link rel="preload" href="https://unpkg.com/leaflet/dist/leaflet.css" />
     <!-- Leaflet JS -->
-    <script src="https://unpkg.com/leaflet/dist/leaflet.js"></script>
+    <script src="https://unpkg.com/leaflet/dist/leaflet.js" defer></script>
 
-    <link rel="stylesheet" href="<?= base_url('assets/ckeditor5-46.0.0/ckeditor5/ckeditor5.css') ?>">
-    <link rel="stylesheet" href="<?= base_url('assets/css/style.css') ?>">
+    <link rel="preload" href="<?= base_url('assets/ckeditor5-46.0.0/ckeditor5/ckeditor5.css') ?>">
+    <link rel="preload" href="<?= base_url('assets/css/style.css') ?>">
 </head>
 
 <body class="w-full" x-data="modalHandler()">
